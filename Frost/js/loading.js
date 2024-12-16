2e729b3f49785c76bc5b6a303e7ab4212d20256f
jieyiy <s111219033@stu.ntue.edu.tw>
2024-12-16 17:11:07 +0800

    loading fade

--- Frost/js/loading.js
+++ Frost/js/loading.js
@@ -1,18 +1,21 @@
 window.onload = function() {
-    const loadingOverlay = document.getElementById('loading-wrapper');
-    const content = document.getElementById('wrapper');
-    
+  const loadingOverlay = document.getElementById('loading-wrapper');
+  const content = document.getElementById('wrapper');
   
-    // 先顯示 loading 層
-    loadingOverlay.style.display = 'flex';
-  
-    // 檢查頁面是否已經完全加載
-    if (document.readyState === 'complete') {
-     
+  // 先显示 loading 层
+  loadingOverlay.style.display = 'flex';
+
+  // 检查页面是否已经完全加载
+  if (document.readyState === 'complete') {
+    setTimeout(() => {
+      // 添加 淡出+圆形缩放 动画类
+      loadingOverlay.classList.add('fade-zoom-out');
+      
+      // 等待动画完成后隐藏 loading 层，显示页面内容
       setTimeout(() => {
-        // 隱藏 loading 層，顯示頁面內容
         loadingOverlay.style.display = 'none';
         content.style.display = 'block';
-      },500);  // 顯示 1 秒鐘，然後隱藏 loading 層
-    }
+      }, 200); // 动画持续时间
+    }, 300);  // 显示 500 毫秒，再开始动画
+  }
 }
